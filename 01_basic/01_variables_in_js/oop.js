52ccbc38a4a4b631571821a72305e819843ef874
alirazaatish <atishaliraza>
2024-05-25 22:16:35 +0500

    upload code

---
+++ 01_basic/01_variables_in_js/oop.js
@@ -0,0 +1,23 @@
+/*
+Varibale in js
+let variable_name
+var variable_name
+const variable_name
+*/
+
+const accountId = 179522;
+let accountEmail = "atishaliraza@gmail.com";
+var accountPassword = "atish12345";
+accountCity = "Skardu";
+
+// accountId ="747833"; Not Allowed!
+accountEmail = "aimen@gmail.com";
+accountPassword = "aimen123";
+accountCity = "Gilgit";
+
+/*
+prefer not to use var 
+because of issue of block scop and functional scop
+*/
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity]); // ya naya sekha hy!
---
+++ 01_basic/02_datatype _in_js/opp.js
@@ -0,0 +1,29 @@
+// Datatype in Javascript
+
+let name = "Ali";  // string type
+let age = 19;   // number type;
+let pass = true; // boolen type
+
+console.log(pass);
+
+//<---Permitive Datatype--->
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolen => true/false
+// null => standalone type
+// undefine => 
+// symbole => unique
+
+//<---Reference Datatype--->
+// Array
+// Object
+// Function/Class
+// Map
+// Set
+
+console.log(typeof "Ahmad");  // string
+
+console.log(typeof(null)); // object
+
+console.log(typeof(undefine)); // undefine  => undefine apna ap ma ak datatype hy
