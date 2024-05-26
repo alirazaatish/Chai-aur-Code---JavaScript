9e68f1e2999735437f904efa3e5c46966f9e6fc9
alirazaatish <atishaliraza>
2024-05-26 09:48:48 +0500

    Add 02_datatype directory: Discuss primitive and non-primitive data types with examples.

--- 01_basic/02_datatype _in_js/opp.js
+++ 01_basic/02_datatype _in_js/opp.js
@@ -27,3 +27,27 @@ console.log(typeof "Ahmad");  // string
 console.log(typeof(null)); // object
 
 console.log(typeof(undefine)); // undefine  => undefine apna ap ma ak datatype hy
+
+//*******************************************************************************************************
+
+
+// Stack(Permitive), Heap(Non-Permitive)
+
+let myGithupName="alirazaatish";
+let anotherName=myGithupName;
+    anotherName="AtishGolpa";
+
+console.log(myGithupName);  // Permitive datatype ma jab change karta hy to actual value ma change nii hota balki copy banta hy
+console.log(anotherName);   // Permitive datatype stack ma store honta hy
+
+let userOne={
+    email: "atish@gmail.com",
+    id: 29404;
+}
+
+let userTwo=userOne  // Non_permitive datatype ma jab value change karta hy to actual value ma change ajata hy
+                    // Non-permitive Heap ma store hota hy
+   userTwo.email="aimen@gmail.com";  
+
+console.log(userOne); // aimen@gmail.com
+console.log(userTwo); // aimen@gmail.com
